7691eb8cb33ee81a7a70fd04d4bf99e50a9a08d2
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-07-11 09:35:17 +0700

    Accuracy with cifar10 is 80%

--- model.py
+++ model.py
@@ -1,30 +1,42 @@
 import tensorflow.keras as keras
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.optimizers import SGD
 import numpy as np
 from tensorflow.keras.callbacks import *
 
 
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+IMAGE_SIZE = X_train.shape[1:]
 
 
 def Model_CNN():
-    input_ = Input(shape = (28, 28, 1))
-    conv_1a = Conv2D(8, kernel_size = (3, 3), padding = 'valid', activation = 'relu')(input_)
-    conv_1b = Conv2D(8, kernel_size = (3, 3), padding = 'valid', activation = 'relu')(conv_1a)
-    maxpool_1 = MaxPooling2D(pool_size = (2, 2), padding = 'valid')(conv_1b)   
+    input_ = Input(shape = IMAGE_SIZE)
+    conv_1a = Conv2D(32, kernel_size = (3, 3), padding = 'same', activation = 'relu')(input_)   
+    conv_1b = Conv2D(32, kernel_size = (3, 3), padding = 'same', activation = 'relu')(conv_1a)
+    maxpool_1 = MaxPooling2D(pool_size = (2, 2))(conv_1b)
+#     norm_1 = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True)(maxpool_1)
+    drop_1 = Dropout(0.25)(maxpool_1)
     
-    conv_2a = Conv2D(16, kernel_size = (3, 3), padding = 'valid', activation = 'relu')(maxpool_1)
-    conv_2b = Conv2D(16, kernel_size = (3, 3), padding = 'valid', activation = 'relu')(conv_2a)
-    maxpool_2 = MaxPooling2D(pool_size = (2, 2), padding = 'valid')(conv_2b)
+    
+#     conv_2z = Conv2D(32, kernel_size = (1, 1), padding = 'same', activation = 'relu')(maxpool_1)
+    conv_2a = Conv2D(64, kernel_size = (3, 3), padding = 'same', activation = 'relu')(drop_1)
+    conv_2b = Conv2D(64, kernel_size = (3, 3), padding = 'same', activation = 'relu')(conv_2a)
+    maxpool_2 = MaxPooling2D(pool_size = (2, 2))(conv_2b)
+    drop_2 = Dropout(0.25)(maxpool_2)
+    
+    conv_3a = Conv2D(128, kernel_size = (3, 3), padding = 'same', activation = 'relu')(drop_2)
+    conv_3b = Conv2D(128, kernel_size = (3, 3), padding = 'same', activation = 'relu')(conv_3a)
+    maxpool_3 = MaxPooling2D(pool_size = (2, 2))(conv_3b)
+    drop_3 = Dropout(0.25)(maxpool_3)
+    average_pooling = AveragePooling2D(pool_size = (4, 4))(drop_3)
 
-    flatten_ = Flatten()(maxpool_2)
-    dense_1 = Dense(128)(flatten_)
-    activation_1 = Activation('relu')(dense_1)
-    dense_2 = Dense(128)(activation_1)
-    activation_2 = Activation('relu')(dense_2)
-    soft_max = Dense(10, activation = 'softmax')(activation_2)
+    flatten_ = Flatten()(average_pooling)
+    dense_1 = Dense(256, activation = 'relu')(flatten_)
+    drop_3 = Dropout(0.25)(dense_1)
+    dense_2 = Dense(128, activation = 'relu')(drop_3)
+    soft_max = Dense(10, activation = 'softmax')(dense_2)
     # soft_max = Activation('softmax')
 
     model = Model(inputs = input_, outputs = soft_max)
@@ -32,11 +44,10 @@ def Model_CNN():
     return model
 
 def my_learning_rate(epoch, lrate):
-    return lrate * (1 / (1 + 1e-3 * epoch))
+    return lrate
 
 model = Model_CNN()
 print(model.summary())
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 # model = Model_VGG16()
 lrs = LearningRateScheduler(my_learning_rate, verbose = 1)
@@ -47,16 +58,16 @@ X_train = X_train / 255
 X_test = X_test / 255
 
 # print(X_train[0].shape)
-X_train = np.expand_dims(X_train, axis = 3)
-X_test = np.expand_dims(X_test, axis = 3)
+# X_train = np.expand_dims(X_train, axis = 3)
+# X_test = np.expand_dims(X_test, axis = 3)
 
-endp = int(len(X_train) * 0.8)
+endp = int(len(X_train) * 0.9)
 
-X_val = X_train[endp:]
-y_val = y_train[endp:]
+# X_val = X_train[endp:]
+# y_val = y_train[endp:]
 
-X_train = X_train[:endp]
-y_train = y_train[:endp]
+# X_train = X_train[:endp]
+# y_train = y_train[:endp]
 # X_train = np.moveaxis(X_train, 0, -1)
 
 print(X_train.shape)
@@ -66,7 +77,7 @@ tsb = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph
                 embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, 
                 update_freq='epoch')
 
-model.fit(X_train, y_train, epochs = 5, callbacks = [lrs, tsb], validation_data = (X_val, y_val))
+model.fit(X_train, y_train, epochs = 25)
 
 loss, score = model.evaluate(X_test, y_test)
 
